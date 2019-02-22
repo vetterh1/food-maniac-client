fe0b0f382b6fa872a3fefbf00549ee5e4d39c9bd
Laurent VETTERHOEFFER <vetterh1@yahoo.fr>
2019-02-22 14:34:25 +0100

    minor changes

--- prod_update_client.sh
+++ prod_update_client.sh
@@ -1,13 +1,12 @@
-cd ./workspace/mapTest/
+cd ./workspace/food-maniac-client/
 rm distFoServer/bundle.*
 rm distFoServer/main.*
 rm distFoServer/manifest.*
 rm distFoServer/styles.*
 rm distFoServer/vendor.*
-pm2 stop all
-pm2 delete boServer foServer
+pm2 stop foServer
+pm2 delete foServer
 git pull
 npm install
-NODE_ENV=production  npm run BoServer:build:prod
-NODE_ENV=production  npm run FoServer:build:prod
+NODE_ENV=production  npm run build:prod
 pm2 start pm2config.json --env production
